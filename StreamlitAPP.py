a89197d9c7e00f5d3b7270752df3778fa73b9168
Patsonu63 <patsonu63@gmail.com>
2024-03-21 18:26:42 +0530

    update

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -3,16 +3,16 @@ import json
 import traceback
 import pandas as pd
 from dotenv import load_dotenv
-from mcqgenerator.utils import read_file,get_table_data
+from mcqgenrator.utils import read_file,get_table_data
 import streamlit as st
 from langchain.callbacks import get_openai_callback
-from mcqgenerator.MCQGenrator import generate_evaluate_chain
-from mcqgenerator.logger import logging
+from mcqgenrator.MCQGenrator import generate_evaluate_chain
+from mcqgenrator.logger import logging
 
 
 #loading json file
 
-with open('Response.json', 'r') as file:
+with open('C:\Users\Sumit\mcqgen1\Response.json', 'r') as file:
     RESPONSE_JSON = json.load(file)
 
 #creating a title for the app
